1932aacd7b1e7f03d020959eb337ac019d6caeff
David Durant <dave@bowsy.co.uk>
2017-06-13 22:00:47 +0100

    Stuff

--- test_trello.py
+++ test_trello.py
@@ -1,7 +1,9 @@
 import numpy as numpy
 from datetime import date, datetime
 from trello import TrelloClient
+from card_plus import CardPlus
 
+# Create connection to Trello
 trello_client = TrelloClient(
     api_key = "daad4d77e580ebd5d2d522f439b07f74",
     api_secret = "c7a8b9ac48bb7074b92161f37f0ccaaf23ee8f165e234bccb86013dd0a28aa19",
@@ -9,14 +11,58 @@ trello_client = TrelloClient(
     token_secret = "129721ad1698d337826dcf66443f7e99"
 )
 
-card_list = trello_client.get_board("7R56bpzE").all_cards()
+# Get the board
+board = trello_client.get_board("7R56bpzE")
 
+# Get all the labels for the board
+label_list = board.get_labels()
+
+# Pull all the cards from the board using the basic Card object
+card_list = board.all_cards()
+
+# Initialise the list of the CardPlus objects which is a suclass of the base Card object that stores more data
+card_plus_list = []
+
+# Convert Card objects to CardPlus objects while adding extra data
 todays_date = date.today()
-
 for card in card_list:
+  
+  # Number of working days since card last updated
   last_updated_date = card.date_last_activity
-  business_days_between = numpy.busday_count(last_updated_date, todays_date)
-  print "Name : " + card.name
-  print "Business_days_between : " + str(business_days_between)
-  print "+++++++"
+  business_days_between = numpy.busday_count(last_updated_date.date(), todays_date)
+  card.business_days_since_update = business_days_between
 
+  # Organisation and registers
+  card.organisation = "None"
+  card.registers = []
+  card.location_picker = False
+  for card_label_id in card.idLabels:
+    for board_label in label_list:
+      if ((card_label_id == board_label.id) and (board_label.color == "green")):
+        card.organisation = board_label.name
+        break
+      elif ((card_label_id == board_label.id) and (board_label.color == "black")):
+        card.registers.append(board_label.name)
+        break
+      elif ((card_label_id == board_label.id) and (board_label.color == "sky")):
+        card.location_picker = True
+        break
+
+
+
+
+# TBD - ADDING MORE DATA TO THE CARD OBJECT BY TOKENISING THE CARD DESCRIPTION!!
+
+
+
+
+
+# For testing
+for card in card_list:
+  print "Name : " + card.name
+  print "Short URL : " + card.short_url
+  print "Business_days_between : " + str(card.business_days_since_update)
+  print "Organisation : " + card.organisation
+  print "Registers : " + ", ".join(card.registers)
+  print "Location picker : True" if (card.location_picker == True) else "Location picker : False"
+  print "+++++++"
